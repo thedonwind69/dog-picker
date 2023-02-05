ac993c62e9c614bd3764a8599ecc7507b29a9aca
thedonwind69@gmail.com <thedonwind69@gmail.com>
2023-02-05 12:33:43 -0600

    edits

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import { dogCategories} from './data';
 
 var currentYear = new Date().getFullYear();
@@ -7,33 +7,56 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-var dogCategories = [
-  "Small Dogs",
-  "Medium Dogs",
-  "Large Dogs"
-]
+function App () {
 
-class App extends React.Component {
+  const [dogCategoriesState, setDogCategoriesState] = useState({categories: ""});
+  const [isLoadingState, setIsLoading] = useState({isLoading: false});
 
-  constructor (props) {
-    super(props);
-    this.state = {
-      dogCategories: dogCategories
-    }
+
+
+  function displayDogCategories () {
+    const displayCategories = dogCategories.map((category) => {
+      var categoryName = category.split(" ").join("");
+      return (
+        <div class="col-4">
+          <div class='card dog-category' >
+            <h1>{category}</h1>
+            <img src="../pics/LargeDogs.jpg" class="img-fluid"></img>
+          </div>
+        </div>
+      )
+    })
+    return displayCategories;
   }
 
-  render () {
+  if (isLoadingState.isLoading) {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Dog Picker {currentYear}!</h1>
-          <h1>{}</h1>
-    
+      <div className='App'>
+        <header class="App-header">
+            <h1>loading....</h1>
         </header>
       </div>
-    );
+    )
+  } else {
+    return (
+      <div className="App">
+          <header className="App-header">
+      
+            <h1 class="dog-picker-title"><strong>DOG PICKER {currentYear}!</strong></h1>
+            
+          {/* display the dog categories */}
+            <div class="container">
+              <div class="row">
+                {displayDogCategories()}
+              </div>
+            </div>
+          {/* .... */}
+          
+          </header>
+      </div>
+    )
   }
+  
 }
-
 
 export default App;
