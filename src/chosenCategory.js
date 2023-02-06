7f510e98bbeace15a37352bb1d1b79dc72d8e541
thedonwind69@gmail.com <thedonwind69@gmail.com>
2023-02-06 17:09:35 -0600

    dog breed

--- src/chosenCategory.js
+++ src/chosenCategory.js
@@ -7,29 +7,48 @@ function ChosenCategory (props) {
     const [chosenBreedState, setChosenBreedState] = useState({chosenBreed: null});
     const [isLoadingState, setIsLoadingState] = useState({isLoading: false});
 
-    return (
-        <div class='App'>
-            <div class="App-header">
-                <div class="container">
-                    <div class='row'>
-                        <div class='col-12 col-lg-6 border border-danger'>
-                            <h1>Your category is {props.chosenCategory}!</h1>
-                            <p>Now click again to choose your specific breed!</p>
-                            <div class=''>
-                                <div class={`${props.chosenCategory.split(" ").join("")}`}>
+    if (isLoadingState.isLoading) {
+        return (
+            <div className='App'>
+                <header class="App-header">
+                    <h1>Picking your dog breed...</h1>
+                    <div class="lds-dual-ring">
 
+                    </div>
+                </header>
+            </div>
+        )
+    } else if (!isLoadingState && chosenBreedState) {
+        return (
+            <ChosenBreed chosenBreed={chosenBreedState.chosenBreed}/>
+        )
+    } else {
+        return (
+            <div class='App'>
+                <div class="App-header">
+                    <div class="container">
+                        <div class='row'>
+                            <div class='col-12 col-lg-6 border border-danger'>
+                                <h1>Your category is {props.chosenCategory}!</h1>
+                                <p>Now click again to choose your specific breed!</p>
+                                <div class=''>
+                                    <div class={`${props.chosenCategory.split(" ").join("")}`}>
+
+                                    </div>
                                 </div>
                             </div>
+                            <div class='col-12 col-lg-6 border border-danger'>
+                                <button type="button" class="btn btn-primary btn-lg">Choose your dog breed!</button>
+                                <button onClick={props.rechooseCategory} type="button" class="btn btn-primary btn-lg">Re-choose category</button>
+                            </div>
                         </div>
-                        <div class='col-12 col-lg-6 border border-danger'>
-                            <button type="button" class="btn btn-primary btn-lg">Choose your dog breed!</button>
-                            <button onClick={props.rechooseCategory} type="button" class="btn btn-primary btn-lg">Re-choose category</button>
-                        </div>
-                    </div>
-                </div> 
+                    </div> 
+                </div>
             </div>
-        </div>
-    )
+        )        
+    }
+
+
 }
 
 
